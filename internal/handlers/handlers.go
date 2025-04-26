62b3ff5fbc447c8e2b748bc40494abea0fc3ebb7
Nick, Nick Kh <nikysoyd@gmail.com>
2025-04-26 11:43:31 +0300

    v2

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"time"
 
-	"your_module_name/service" // Замените на актуальный путь к пакету service
+	"github.com/nikysoyd/sprint6/service" // Замените на актуальный путь к пакету service
 )
 
 // IndexHandler обрабатывает запрос к корневому эндпоинту /
