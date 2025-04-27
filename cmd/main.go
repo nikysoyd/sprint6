d37221ed532552525b1dc94b4b6dd3808fae300c
Nick, Nick Kh <nikysoyd@gmail.com>
2025-04-27 18:02:16 +0300

    1755

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/nikysoyd/sprint6/internal/server"
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
 )
 
 func main() {
 	// Создаём логгер
-	logger := log.New(os.Stderr, "ERROR: ", log.LstdFlags|log.Lshortfile)
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
 
-	// Создаём сервер (теперь с обработкой ошибки)
-	srv, err := server.NewServer(logger)
-	if err != nil {
-		logger.Fatalf("Failed to create server: %v", err)
-	}
+	// Создаём сервер
+	srv := server.New(logger)
 
 	// Запускаем сервер
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Server failed to start: %v", err)
+	logger.Println("Сервер запущен на http://localhost:8080")
+	if err := srv.HTTP.ListenAndServe(); err != nil {
+		logger.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/nikysoyd/sprint6
 
-go 1.24
+go 1.24.0
+
+toolchain go1.24.2
+
+require github.com/Yandex-Practicum/go1fl-sprint6-final v0.0.0-20250408191652-615c949951af // indirect
